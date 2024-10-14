4bbae42de0cb1f1a10c8ac23778d616954ed7ff5
Anže Oblak <anze.oblak@rettro.si>
2024-10-14 15:43:16 +0200

    Revert "display ranks in box plot"
    This reverts commit aba497fe92bf16346f44d293aacb8e9a52bfa2c2.

--- compare_search_methods.py
+++ compare_search_methods.py
@@ -1,12 +1,10 @@
 import yaml
 import search_tfidf as search_tfidf
 import search_sBERT as search_sbert
-import matplotlib.pyplot as plt
 
 
-def get_tfidf_performance():
+def get_tfidf_avg_rank():
     avg_rank = 0
-    ranks = []
 
     enote, tfidf_matrix, vectorizer = search_tfidf.prepare_data()
 
@@ -22,14 +20,12 @@ def get_tfidf_performance():
             target_unit_rank += 1
 
         avg_rank += target_unit_rank
-        ranks.append(target_unit_rank)
 
-    return avg_rank / len(test_data), ranks
+    return avg_rank / len(test_data)
 
 
-def get_sbert_performance():
+def get_sbert_avg_rank():
     avg_rank = 0
-    ranks = []
 
     enote, preprocessed_enote_embeddings = search_sbert.prepare_data()
     
@@ -45,30 +41,16 @@ def get_sbert_performance():
             target_unit_rank += 1
 
         avg_rank += target_unit_rank
-        ranks.append(target_unit_rank)
 
-    return avg_rank / len(test_data), ranks
+    return avg_rank / len(test_data)
 
 
 if __name__ == '__main__':
     with open('test_set.yaml', 'r') as file:
         test_data = yaml.safe_load(file)    
 
-    tfidf_avg_rank, tfidf_ranks = get_tfidf_performance()
-    sbert_avg_rank, sbert_ranks = get_sbert_performance()
+    tfidf_avg_rank = get_tfidf_avg_rank()
+    sbert_avg_rank = get_sbert_avg_rank()
 
     print(f"Povprečen rank ciljne enote s TF-IDF: {tfidf_avg_rank:.2f}")
     print(f"Povprečen rank ciljne enote z sBERT: {sbert_avg_rank:.2f}")
-
-    plt.figure(figsize=(8, 6))  # Set the figure size
-
-    # Use boxplot function to create the plot with labels
-    plt.boxplot([tfidf_ranks, sbert_ranks], patch_artist=True, tick_labels=['TF-IDF', 'sBERT'])
-
-    # Add title and labels
-    plt.title('Primerjava povprečnega ranka ciljne enote')
-    plt.ylabel('Rank')
-    plt.xlabel('Metoda')
-
-    # Display the plot
-    plt.show()
