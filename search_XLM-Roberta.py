fc579bcbc96f37dd94d325b08432731ccd27bff0
Anže Oblak <anze.oblak@rettro.si>
2025-01-06 17:55:38 +0100

    search relevant units using XLM-Roberta model with SentenceTransformer

---
+++ search_XLM-Roberta.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import yaml
+
+EMBEDDINGS_FILE = 'embeddings/XLM-ROBERTA/embeddings.npy'
+
+# THE MODEL IS NOW DEPRECATED!!!!!!!
+model = SentenceTransformer('xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+
+def chunk_text(text, chunk_size=512):
+    words = text.split()
+    chunks = [words[i:i + chunk_size] for i in range(0, len(words), chunk_size)]
+    return [' '.join(chunk) for chunk in chunks]
+
+def preprocess(text):
+    # # Split text into chunks and encode each chunk
+    # chunks = chunk_text(text)
+    # chunk_embeddings = model.encode(chunks)
+    # # Average the embeddings across all chunks to get a single representation for the text
+    # return np.mean(chunk_embeddings, axis=0)
+    return model.encode(text)
+
+def search(query, embeddings, top_n=None):
+    query_embedding = model.encode(query)
+
+    similarity_scores = model.similarity(query_embedding, embeddings).numpy().flatten()
+
+    if top_n == None:
+        # Take all relevant results
+        top_indices = np.argsort(similarity_scores)[::-1]
+    else:
+        # Take only the top n relevant results
+        top_indices = np.argsort(similarity_scores)[-top_n:][::-1]
+
+    return top_indices, similarity_scores[top_indices]
+
+
+def prepare_data():
+    with open('ai_act.yaml', 'r') as file:
+        data = yaml.safe_load(file)
+
+    cleni = [f"{d['id_elementa']}" for d in data['cleni']]
+    tocke = [f"{d['id_elementa']}" for d in data['tocke']]
+    enote = cleni + tocke
+
+    if os.path.exists(EMBEDDINGS_FILE):
+        print("Loading existing embeddings...")
+        preprocessed_enote_embeddings = np.load(EMBEDDINGS_FILE)
+    else:
+        print("Getting new embeddings and storing them to file...\n")
+
+        preprocessed_cleni_embeddings = [
+            preprocess(
+                d['poglavje']['naslov'] + "\n" + 
+                (d['oddelek']['naslov'] + "\n" if d['oddelek'] else '') + 
+                d['naslov'] + "\n" + 
+                d['vsebina']
+            ) for d in data['cleni']]
+        
+        preprocessed_tocke_embeddings = [
+            preprocess(d['vsebina']) for d in data['tocke']
+        ]
+
+        preprocessed_enote_embeddings = preprocessed_cleni_embeddings + preprocessed_tocke_embeddings
+
+        if not os.path.exists(os.path.dirname(EMBEDDINGS_FILE)):
+            os.makedirs(os.path.dirname(EMBEDDINGS_FILE), exist_ok=True)
+
+        np.save(EMBEDDINGS_FILE, preprocessed_enote_embeddings)
+
+    return enote, np.array(preprocessed_enote_embeddings) 
+
+
+def get_relevant_results(query="Katere zahteve morajo izpolnjevati visokotvegani sistemi UI v zvezi s preglednostjo in zagotavljanjem informacij uvajalcem?", top_n=None):
+    enote, preprocessed_enote_embeddings = prepare_data()
+
+    top_indices, scores = search(query, preprocessed_enote_embeddings, top_n)
+
+    print("Relevantne enote:")
+    for idx, score in zip(top_indices, scores):
+        print(f"{enote[idx]} s podobnostjo {score}")
